3d9d55e98311bc33e0af8920d7b8974302142d20
selcuksezer <selcuksezer6@gmail.com>
2022-02-16 01:19:16 +0300

    Added features to places list screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:lucis/screens/places_list_screen.dart';
+
+void main() => runApp(const Lucis());
+
+class Lucis extends StatelessWidget {
+  const Lucis({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData(primarySwatch: Colors.indigo);
+    return MaterialApp(
+      home: PlacesListScreen(),
+      title: 'Lucis',
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          secondary: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
